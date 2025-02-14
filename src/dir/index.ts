8fc753e5b0582111ac864c4f3c35a44489dfadf0
zyjared <zyjared@outlook.com>
2025-02-14 13:44:17 +0800

    refactor: params

--- src/dir/index.ts
+++ src/dir/index.ts
@@ -4,6 +4,8 @@ import { getDirsMap } from './map'
 import { initOptions } from './opts'
 import { step } from './step'
 
+export type { Options } from './opts'
+
 export function defineSidebar(options: Options = {}) {
   const opts = initOptions(options)
   const dirsmap = getDirsMap(opts)
